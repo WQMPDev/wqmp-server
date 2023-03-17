b38117d25939dfc0b52a8467a59eef9ec062a09a
SteveBeeblebrox <41750582+SteveBeeblebrox@users.noreply.github.com>
2023-03-16 21:26:44 -0600

    CSV

--- api/get.js
+++ api/get.js
@@ -1,11 +1,16 @@
 import { connect } from '@planetscale/database'
+import { CSV } from './csv.min.js'
 export default async function handler(request, response) {
-    console.log(process.env['DATABASE_URL'])
     const config = {
         url: process.env['DATABASE_URL']
     }
 
-    const conn = connect(config)
-    const results = await conn.execute('select * from Records', [])
-    return response.send(results);
+    const conn = connect(config);
+    const result = await conn.execute('select * from Records', []);
+    return CSV.stringify(result.rows.map(o=>{delete o.RecordId; delete o.DeviceId; return o;}), function(key, value) {
+        if(key === null) {
+            return value.toLowerCase();
+        }
+        return value;
+    });
 }
