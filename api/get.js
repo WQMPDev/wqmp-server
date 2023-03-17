27f47c8a88f555ea83fa2594cd89cc409b981851
SteveBeeblebrox <41750582+SteveBeeblebrox@users.noreply.github.com>
2023-03-16 21:44:13 -0600

    Update get.js

--- api/get.js
+++ api/get.js
@@ -5,12 +5,16 @@ export default async function handler(request, response) {
         url: process.env['DATABASE_URL']
     }
 
+    const {deviceId = 'Sample-Data'} = request.query;
+
     const conn = connect(config);
-    const result = await conn.execute('select * from Records', []);
-    return CSV.stringify(result.rows.map(o=>{delete o.RecordId; delete o.DeviceId; return o;}), function(key, value) {
+    const result = await conn.execute('select * from Records where DeviceId = ?', [deviceId]);
+    
+    response.setHeader('Content-Type', 'text/csv');
+    return response.send(CSV.stringify(result.rows.map(o=>{delete o.RecordId; delete o.DeviceId; return o;}), function(key, value) {
         if(key === null) {
             return value.toLowerCase();
         }
         return value;
-    });
+    }));
 }
