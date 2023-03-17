5c683371d1bb43e5c4204d10e0cba1a50db5db32
SteveBeeblebrox <41750582+SteveBeeblebrox@users.noreply.github.com>
2023-03-16 18:01:53 -0600

    Update

--- api/get.ts
+++ api/get.ts
@@ -1,4 +1,11 @@
-export default function handler(req, res) {
-    const { name = 'World' } = req.query;
-    return res.send(`Got ${name}!`);
+import { connect } from '@planetscale/database'
+export default async function handler(req, res) {
+
+    const config = {
+        url: process.env['DATABASE_URL']
+    }
+
+    const conn = connect(config)
+    const results = await conn.execute('select 1 from Records', [1])
+    return res.send(results);
 }
