1015331f0b8b56939b7bf309d85bdc58cdc5b553
SteveBeeblebrox <41750582+SteveBeeblebrox@users.noreply.github.com>
2023-03-16 21:02:32 -0600

    Update

--- api/get.js
+++ api/get.js
@@ -1,11 +1,13 @@
 import { connect } from '@planetscale/database'
-export default async function handler(req, res) {
+export default async function handler(request, response) {
 
     const config = {
         url: process.env['DATABASE_URL']
     }
 
-    const conn = connect(config)
-    const results = await conn.execute('select 1 from Records', [])
-    return res.send(results);
+    response.send(config.url ?? 'unknown')
+
+    //const conn = connect(config)
+    //const results = await conn.execute('select 1 from Records', [])
+    //return res.send(results);
 }
